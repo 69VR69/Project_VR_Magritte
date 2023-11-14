44f249364f4dbc15483da86c8a96eb0d45724e52
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-11-14 09:53:51 -0800

    Add InputManager

---
+++ Assets/Script/InputTestor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class InputTestor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _TriggerCube;
+    [SerializeField]
+    private GameObject _BackButtonCube;
+    [SerializeField]
+    private GameObject _TouchPadCube;
+    [SerializeField]
+    private InputManager _inputManager;
+
+    private void Update()
+    {
+        _TriggerCube.SetActive(_inputManager.IsTrigger);
+        _BackButtonCube.SetActive(_inputManager.IsBackButton);
+
+        var renderer = _TouchPadCube.GetComponent<Renderer>();
+        var val = _inputManager.TouchPad;
+        // Create a color from Vector2
+        renderer.material.color = new Color(val.x, val.y, 0);
+    }
+}
