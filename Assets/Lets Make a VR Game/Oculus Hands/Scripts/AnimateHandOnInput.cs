f798e73fd61058eb866ee6b8d8555de8ea466fdf
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-11-16 21:01:18 -0800

    Add new a=hand + first start animation

--- Assets/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
+++ Assets/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
 public class AnimateHandOnInput : MonoBehaviour
 {
-    public InputActionProperty pinchAnimationAction;
-    public InputActionProperty gripAnimationAction;
-    public Animator handAnimator;
+    [SerializeField]
+    private Animator _handAnimator;
+    private InputManager _inputManager;
+
+    private void Awake() => _inputManager = InputManager.Instance;
 
     // Update is called once per frame
     void Update()
     {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
-
-        float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
+        _handAnimator.SetBool("point", _inputManager.IsTrigger);
+        _handAnimator.SetBool("grab", _inputManager.IsBackButton);
     }
 }
