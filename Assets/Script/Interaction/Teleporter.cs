65c6dfc1b7446ba2c7e87a1f72a428f941321119
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-01-10 13:54:49 +0100

    Refactor projet + Build scene manager

---
+++ Assets/Script/Interaction/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Script.Interaction
+{
+    public class Teleporter : Interactable
+    {
+        public override void Run(GameObject sender, InputManager inputManager)
+        {
+            base.Run(sender, inputManager);
+
+            if (inputManager.IsTrigger)
+            {
+                // Do the teleportation
+            }
+        }
+    }
+}
