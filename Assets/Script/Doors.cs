78638900feb6d1b8d8655841a36c89ffe1ba7bf6
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-01-18 16:08:55 +0100

    Fix doors

--- Assets/Script/Doors.cs
+++ Assets/Script/Doors.cs
@@ -1,38 +1,40 @@
 using UnityEngine;
 using System.Collections;
 
-public class Doors : MonoBehaviour {
+public class Doors : MonoBehaviour
+{
 
-	Animator animator;
-	bool doorOpen;
+    Animator animator;
+    bool doorOpen;
 
-	void Start()
-	{
-		doorOpen = false;
-		animator = GetComponent<Animator>();
-	}
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
 
-	void OnTriggerEnter(Collider col)
-	{
-		Debug.Log("test doors");
-		if (col.gameObject.tag == "Player") {
-	
-			doorOpen = true;
-			DoorControl ("Open");
-		}
-	
-	}
-	void OnTriggerExit(Collider col)
-	{
-		if (doorOpen) 
-		{
-			doorOpen = false; 
-			DoorControl ("Close");
-		}
-	}
-	void DoorControl(string direction)
-	{
-		animator.SetTrigger(direction);
-	}
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("test doors");
+        if (col.gameObject.tag == "Player")
+        {
+
+            doorOpen = true;
+            DoorControl("Open");
+        }
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("Close");
+        }
+    }
+    void DoorControl(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
 
 }
