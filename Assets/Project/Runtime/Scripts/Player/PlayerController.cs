44fc0499051ad976b839144111d554f7903fd18e
lina-bkd <lina.nes@icloud.com>
2024-02-05 21:30:08 +0100

    fix movement test

--- Assets/Project/Runtime/Scripts/Player/PlayerController.cs
+++ Assets/Project/Runtime/Scripts/Player/PlayerController.cs
@@ -1,3 +1,58 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [RequireComponent(typeof(PlayerMotor))]
+// public class PlayerController : MonoBehaviour
+// {
+//     [SerializeField]
+//     private float speed = 3f;
+//     [SerializeField]
+//     private float mouseSensitivity = 3f;
+//     [SerializeField]
+//     private InputManager _inputManager;
+
+//     private PlayerMotor motor;
+//     private Camera _camera;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         motor = GetComponent<PlayerMotor>();
+//         _inputManager ??= InputManager.Instance;
+//         _camera = Camera.main;
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+
+//         var val = _inputManager.TouchPad;
+
+//         //Calculer la vitesse du mouvement du joueur 
+//         //float xMov = Input.GetAxisRaw("Horizontal");
+//         //float yMov = Input.GetAxisRaw("Vertical");
+//         float xMov = val.x;
+//         float yMov = val.y;
+
+//         Vector3 moveHorizontal = transform.right * xMov;
+//         Vector3 moveVertical = transform.forward * yMov;
+
+//         Vector3 velocity = (moveVertical + moveHorizontal).normalized * speed;
+
+//         motor.Move(velocity);
+
+//         //calcul de la rotation du routeur en un Vector3
+//         Quaternion _rot = _camera.transform.rotation;
+
+//         motor.Rotate(_rot);
+           
+//     }
+
+//     public void DisableMovement()
+//     {
+//         speed = 0;
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +69,8 @@ public class PlayerController : MonoBehaviour
 
     private PlayerMotor motor;
     private Camera _camera;
-    // Start is called before the first frame update
+    private bool canMove = true;
+
     void Start()
     {
         motor = GetComponent<PlayerMotor>();
@@ -22,34 +78,42 @@ public class PlayerController : MonoBehaviour
         _camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (canMove)
+        {
+            var val = _inputManager.TouchPad;
 
-        var val = _inputManager.TouchPad;
+            float xMov = val.x;
+            float yMov = val.y;
 
-        //Calculer la vitesse du mouvement du joueur 
-        //float xMov = Input.GetAxisRaw("Horizontal");
-        //float yMov = Input.GetAxisRaw("Vertical");
-        float xMov = val.x;
-        float yMov = val.y;
+            Vector3 cameraForward = _camera.transform.forward;
+            Vector3 cameraRight = _camera.transform.right;
 
-        Vector3 moveHorizontal = transform.right * xMov;
-        Vector3 moveVertical = transform.forward * yMov;
+            // Calcul de la direction du mouvement dans l'espace du monde
+            Vector3 moveDirection = (cameraForward * yMov + cameraRight * xMov).normalized;
 
-        Vector3 velocity = (moveVertical + moveHorizontal).normalized * speed;
+            Vector3 velocity = moveDirection * speed;
 
-        motor.Move(velocity);
+            motor.Move(velocity * Time.deltaTime);
 
-        //calcul de la rotation du routeur en un Vector3
-        Quaternion _rot = _camera.transform.rotation;
-
-        motor.Rotate(_rot);
-           
+            // // Rotation du joueur en fonction de la direction du mouvement
+            // if (moveDirection != Vector3.zero)
+            // {
+            //     Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            //     motor.Rotate(toRotation);
+            // }
+        }
     }
 
     public void DisableMovement()
     {
-        speed = 0;
+        canMove = false;
+    }
+
+    public void EnableMovement()
+    {
+        canMove = true;
     }
 }
+
--- Assets/Project/Runtime/Scripts/Player/PlayerMotor.cs
+++ Assets/Project/Runtime/Scripts/Player/PlayerMotor.cs
@@ -1,3 +1,58 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [RequireComponent(typeof(Rigidbody))]
+// public class PlayerMotor : MonoBehaviour
+// {
+//     private Vector3 velocity;
+//     private Quaternion rotation;
+//     private Rigidbody rb;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//     }
+
+//     public void Move(Vector3 _velocity)
+//     {
+//         velocity = _velocity;
+//     }
+
+//     public void Rotate(Quaternion _rotation)
+//     {
+//         rotation = _rotation;
+//     }
+
+//     private void FixedUpdate()
+//     {
+//         performMovement();
+//         performRotation();
+//     }
+
+//     private void performMovement()
+//     {
+//         if(velocity != Vector3.zero)
+//         {
+//             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+//         }
+//         rb.velocity = Vector3.zero;
+//     }
+
+//     private void performRotation()
+//     {
+//         rb.MoveRotation(rotation);
+
+//     }
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+
+
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +61,8 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     private Vector3 velocity;
-    private Quaternion rotation;
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,36 +73,22 @@ public class PlayerMotor : MonoBehaviour
         velocity = _velocity;
     }
 
-    public void Rotate(Quaternion _rotation)
+    public void Rotate(Quaternion toRotation)
     {
-        rotation = _rotation;
+        rb.MoveRotation(toRotation);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        performMovement();
-        performRotation();
+        PerformMovement();
     }
 
-    private void performMovement()
+    private void PerformMovement()
     {
-        if(velocity != Vector3.zero)
+        if (velocity != Vector3.zero)
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
         }
         rb.velocity = Vector3.zero;
     }
-
-    private void performRotation()
-    {
-        rb.MoveRotation(rotation);
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 }
